3da8badbe9505e205834529168158428c80cd91b
Harhat18 <harfat1909@gmail.com>
2024-10-20 20:20:20 +0300

    update

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -73,13 +73,17 @@ export const verifyCode = async (req: Request, res: Response) => {
       return;
     }
     if (user && user.deviceId !== deviceId) {
-      if (user.socketId) {
-        io.on('connection', (socket) => {
+      io.on('connection', (socket) => {
+        if (user.socketId) {
+          io.to(user.socketId).emit('logout', {
+            message: 'Başka bir cihazdan giriş yapıldı',
+          });
           socket.on('disconnect', () => {
             console.log('Kullanıcı bağlantıyı kapattı');
           });
-        });
-      }
+        }
+      });
+
       user.socketId = socketId;
       user.deviceId = deviceId;
       await user.save();
