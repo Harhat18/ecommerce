d85791ffc45e1aa5fa4ece2297ffc425c5295ca4
Harhat18 <harfat1909@gmail.com>
2024-10-23 23:38:59 +0300

    update

--- src/controllers/connection.controller.ts
+++ src/controllers/connection.controller.ts
@@ -15,7 +15,7 @@ export const sendConnectionRequest = async (
     const receiver = await User.findOne({ phoneNumber: receiverPhoneNumber });
 
     if (!sender || !receiver) {
-      res.status(404).json({ message: 'Kullanıcı bulunamadı' });
+      res.status(200).json({ errMessage: 'Kullanıcı bulunamadı' });
       return;
     }
 
@@ -24,7 +24,7 @@ export const sendConnectionRequest = async (
       receiver: receiver._id,
     });
     if (existingRequest) {
-      res.status(400).json({ message: 'Zaten bir istek gönderdiniz' });
+      res.status(200).json({ errMessage: 'Zaten bir istek gönderdiniz' });
       return;
     }
 
@@ -46,7 +46,7 @@ export const sendConnectionRequest = async (
       .status(201)
       .json({ message: 'Bağlantı isteği gönderildi', request: newRequest });
   } catch (error) {
-    res.status(500).json({ message: 'Sunucu hatası', error });
+    res.status(500).json({ errMessage: 'Sunucu hatası', error });
   }
 };
 
@@ -61,7 +61,7 @@ export const respondToRequest = async (
       .populate('sender')
       .populate('receiver');
     if (!request) {
-      res.status(404).json({ message: 'İstek bulunamadı' });
+      res.status(404).json({ errMessage: 'İstek bulunamadı' });
       return;
     }
     const sender = request.sender as any;
@@ -72,7 +72,7 @@ export const respondToRequest = async (
     } else if (action === 'reject') {
       request.status = 'rejected';
     } else {
-      res.status(400).json({ message: 'Geçersiz işlem' });
+      res.status(400).json({ errMessage: 'Geçersiz işlem' });
       return;
     }
 
@@ -95,7 +95,7 @@ export const respondToRequest = async (
       .status(200)
       .json({ message: `İstek ${request.status} edildi`, request });
   } catch (error) {
-    res.status(500).json({ message: 'Sunucu hatası', error });
+    res.status(500).json({ errMessage: 'Sunucu hatası', error });
   }
 };
 
@@ -108,13 +108,13 @@ export const deleteConnection = async (
 
     const connection = await ConnectionRequest.findById(connectionId);
     if (!connection) {
-      res.status(404).json({ message: 'Bağlantı bulunamadı' });
+      res.status(404).json({ errMessage: 'Bağlantı bulunamadı' });
       return;
     }
 
     await ConnectionRequest.findByIdAndDelete(connectionId);
     res.status(200).json({ message: 'Bağlantı silindi' });
   } catch (error) {
-    res.status(500).json({ message: 'Sunucu hatası', error });
+    res.status(500).json({ errMessage: 'Sunucu hatası', error });
   }
 };
